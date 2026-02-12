3dd27dc5623e55f0365145727ba63abf192ee1cc
Jiko <youstinahabib422@gmail.com>
2026-02-12 18:47:57 +0200

    add figure logis -2- - create supa_figure_data_source inside feature's data layer

---
+++ lib/features/figure_crud/data/data_sources/supa_figure_data_source.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:timelens_dashboard/constants.dart';
+
+class SupabaseEraDataSource {
+  final SupabaseClient supabase;
+
+  SupabaseEraDataSource(this.supabase);
+
+  Future<void> insertEra(Map<String, dynamic> data) async {
+    try {
+      log (data.toString());
+      await supabase.from(kSupaFiguresTable).insert(data);
+
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
